4e21660f205fa800bb2ddf04c21a03ac741d1b9e
irvin pineda <62679993+irvinpinedaUCA@users.noreply.github.com>
2021-11-29 10:56:14 -0600

    Updates

---
+++ precache-manifest.bb227b29e40e9ef729de8f0b70842f9e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f50487c0e49c01a869644f31e2f2bc4",
+    "url": "/FinalProjectTest/index.html"
+  },
+  {
+    "revision": "524aa3231aef7f451f0b",
+    "url": "/FinalProjectTest/static/css/main.b07ab149.chunk.css"
+  },
+  {
+    "revision": "b394c1143b3a921620b5",
+    "url": "/FinalProjectTest/static/js/2.f2dcd7ef.chunk.js"
+  },
+  {
+    "revision": "0569f079e111a6d4f0bf8418a634cb07",
+    "url": "/FinalProjectTest/static/js/2.f2dcd7ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31e23b798db19b14ba16",
+    "url": "/FinalProjectTest/static/js/3.76126ac3.chunk.js"
+  },
+  {
+    "revision": "524aa3231aef7f451f0b",
+    "url": "/FinalProjectTest/static/js/main.dcf2ec16.chunk.js"
+  },
+  {
+    "revision": "0cf9d5c103f3b875b3e9",
+    "url": "/FinalProjectTest/static/js/runtime-main.eb499352.js"
+  },
+  {
+    "revision": "49c53331d19be74b52d47fcce7e97468",
+    "url": "/FinalProjectTest/static/media/programmer.49c53331.png"
+  }
+]);
